d3765ebebb2663f03417ffa2f5e0fccd579b6e73
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-03 10:13:34 -0300

    Adicionados style para a LoginView e custom renderer para componente entry

--- JobSearch.App/JobSearch.App/AssemblyInfo.cs
+++ JobSearch.App/JobSearch.App/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat")]
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "MontserratLight")]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "MontserratBold")]
