f3a04c433f2e16dbc3b33960bfca155c3835401b
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-08 10:28:47 -0300

    Adicionado busca de vagas em StartView, através de CollectionView

--- JobSearch.App/JobSearch.App/Services/JobService.cs
+++ JobSearch.App/JobSearch.App/Services/JobService.cs
@@ -11,23 +11,36 @@ namespace JobSearch.App.Services
 {
     public class JobService : Service
     {
-        public async Task<IEnumerable<Job>> GetJobs(string word, string cityState, int numberOfPage = 1)
+        public async Task<ResponseService<List<Job>>> GetJobs(string word, string cityState, int numberOfPage = 1)
         {
-            // Registra a response gerada pelo método get feito no enderço indicado
             HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/api/jobs?word={word}" +
                                                 $"&cityState={cityState}&numberOfPage={numberOfPage}");
 
-            List<Job> list = null;
+            ResponseService<List<Job>> responseService = new ResponseService<List<Job>>();
+            responseService.IsSucess = response.IsSuccessStatusCode;
+            responseService.StatusCode = (int)response.StatusCode;
+
+
             if (response.IsSuccessStatusCode)
             {
+                //Define 'job' como resultado da request 
+                responseService.Data = await response.Content.ReadAsAsync<List<Job>>();
 
-                //Define 'list' como resultado da request 
-                list = await response.Content.ReadAsAsync<List<Job>>();
             }
-            return list;
+            else
+            {
+                String problemResponse = await response.Content.ReadAsStringAsync();
+                var errors = JsonConvert.DeserializeObject<ResponseService<List<Job>>>(problemResponse);
+
+                responseService.Errors = errors.Errors;
+            }
+            //retorna usuário nulo ou resultado da request
+            return responseService;
+
+            
         }
 
-        public async Task<Job> GetJob(int id)
+        public async Task<ResponseService<Job>> GetJob(int id)
         {
             // Registra a response gerada pelo método get feito no enderço indicado
             HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/api/Job");
@@ -53,21 +66,6 @@ namespace JobSearch.App.Services
             //retorna usuário nulo ou resultado da request
             return responseService;
 
-
-
-
-
-            // Registra a response gerada pelo método get feito no enderço indicado
-            HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/ap/Jobs/1");
-
-            Job job = null;
-            if (response.IsSuccessStatusCode)
-            {
-                //Define 'job' como resultado da request 
-                job = await response.Content.ReadAsAsync<Job>();
-            }
-            //retorna job nulo ou resultado da request
-            return job;
         }
         public async Task<ResponseService<Job>> AddJob(Job job)
         {
--- JobSearch.App/JobSearch.App/Views/StartView.xaml.cs
+++ JobSearch.App/JobSearch.App/Views/StartView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using JobSearch.App.Models;
+using JobSearch.App.Services;
+using JobSearch.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,14 @@ namespace JobSearch.App.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StartView : ContentPage
     {
+        private JobService _service;
+        private ObservableCollection<Job> _listOfJobs;
+        private SearchParams _searchParams;
         public StartView()
         {
             InitializeComponent();
+
+            _service = new JobService();
         }
 
         private void GoVisualizer(object sender, EventArgs e)
@@ -42,6 +51,56 @@ namespace JobSearch.App.Views
             App.Current.Properties.Remove("User");
             App.Current.SavePropertiesAsync();
             App.Current.MainPage = new LoginView();
+        }
+
+        private async void Search(object sender, EventArgs e)
+        {
+            // Coleta os dados informados pelo o usuário
+            string word = TxtWord.Text;
+            string cityState = TxtCityState.Text;
+
+            // Intancia um objeto e define os parametros como os dados salvos anteriormente
+            _searchParams = new SearchParams() { Word = word, CityState = cityState, NumberOfPage = 1 };
+
+            // Preenche a lista de vagas com o response do método GetJobs, usando os parametros do objeto
+            ResponseService<List<Job>> responseService = await _service.GetJobs(_searchParams.Word, 
+                                                         _searchParams.CityState, _searchParams.NumberOfPage);
+
+            if (responseService.IsSucess)
+            {
+                // Instancia uma ObservableCollection utilizando os dados da responseService
+                _listOfJobs = new ObservableCollection<Job>( responseService.Data );
+                // preenche a CollectionView com as instancias
+                ListOfJobs.ItemsSource = _listOfJobs;
+                ListOfJobs.RemainingItemsThreshold = 1;
+            }
+            else
+            {
+                await DisplayAlert("Erro!", "Oops! Ocorreu um erro inesperado! Tente novamente mais tarde.", "OK");
+
+            }
+        }
+
+        private async void InfinitySearch(object sender, EventArgs e)
+        {
+            _searchParams.NumberOfPage++;
+
+            ResponseService<List<Job>> responseService = await _service.GetJobs(_searchParams.Word, _searchParams.CityState, _searchParams.NumberOfPage);
+
+            if (responseService.IsSucess)
+            {
+                // Colocar dentro da CollectionView
+                var listOfJobsFromService = responseService.Data;
+                foreach (var item in listOfJobsFromService)
+                {
+                    _listOfJobs.Add(item);
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro!", "Oops! Ocorreu um erro inesperado! Tente novamente mais tarde.", "OK");
+
+            }
         }
     }
 }
